5b1245e1905c781b29d83099ea1884a21e41c343
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-09-21 17:18:55 +0300

    Project customization

---
+++ HomeworkEleven/Sources/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - UI
+
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Setups
+    
+    private func setupView() {
+        
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Actions
+
+}
