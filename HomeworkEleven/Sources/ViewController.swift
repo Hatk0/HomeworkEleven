c35a79c8f28425771728dc9f2c9d7a59e5803dff
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-09-21 17:28:20 +0300

    Add UIImageView backgroundImage

--- HomeworkEleven/Sources/ViewController.swift
+++ HomeworkEleven/Sources/ViewController.swift
@@ -3,26 +3,39 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - UI
-
+    private lazy var backgroundImage: UIImageView = {
+       let image = UIImage(named: "backgroundImage")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
 
     // MARK: - Setups
     
     private func setupView() {
-        
+        view.backgroundColor = .white
     }
     
     private func setupHierarchy() {
-        
+        view.addSubview(backgroundImage)
     }
     
     private func setupLayout() {
-        
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
     
     // MARK: - Actions
